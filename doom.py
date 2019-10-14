1cd5164c049dcad093ef2acc13bb028b328390a6
Jayacharan Kolla <Jayacharan.Kolla@ttu.edu>
2019-10-14 12:44:09 -0500

    Update doom.py

--- doom.py
+++ doom.py
@@ -8,3 +8,14 @@ import os
 import sys
 import timeit
 from vizdoom import *
+
+def get_input_shape(Image,Filter,Stride):
+  layer1 = math.ceil(((Image - Filter + 1) / Stride))
+  out1 = math.ceil((layer1 / Stride))
+    
+  layer2 = math.ceil(((o1 - Filter + 1) / Stride))
+  out2 = math.ceil((layer2 / Stride))
+    
+  layer3 = math.ceil(((o2 - Filter + 1) / Stride))
+  out3 = math.ceil((layer3  / Stride))
+  return int(o3)
