9eca02bcc238ae76a7b0ae831da39bfc318b4cd2
Shubham Dhyani <shubhamdhyani1992@gmail.com>
2019-12-01 19:05:11 -0600

    Update doom_game.py
    Updating parameters

--- src/doom_game.py
+++ src/doom_game.py
@@ -33,61 +33,61 @@ def get_input_shape(Image,Filter,Stride):
 class DRQN():
     def __init__(self, input_shape, num_actions, initial_learning_rate):
         
-        # first, we initialize all the hyperparameters
+        # initializing all the hyperparameters
 
         self.tfcast_type = tf.float32
         
-        # shape of our input, which would be (length, width, channels)
+        # deining the shape of our input, which would be (length, width, channels)
         self.input_shape = input_shape 
         
-        # number of actions in the environment
+        # defining the number of actions in the environment
         self.num_actions = num_actions
         
-        # learning rate for the neural network
+        # defining the learning rate for the neural network
         self.learning_rate = initial_learning_rate
                 
-        # now we will define the hyperparameters of the convolutional neural network 
+        # defining the hyperparameters of the convolutional neural network 
 
-        # filter size
+        # setting the filter size
         self.filter_size = 5
         
-        # number of filters
+        # setting the number of filters
         self.num_filters = [16, 32, 64]
         
-        # stride size
+        # setting up the stride size
         self.stride = 2
         
-        # pool size
+        # setting up the pool size
         self.poolsize = 2 
         
-        # shape of our convolutional layer
+        # setting up the shape of our convolutional layer
         self.convolution_shape = get_input_shape(input_shape[0], self.filter_size, self.stride) * get_input_shape(input_shape[1], self.filter_size, self.stride) * self.num_filters[2]
         
-        # now, we define the hyperparameters of our recurrent neural network and the final feed forward layer
+        #defining the hyperparameters of our recurrent neural network and the final feed forward layer
         
-        # number of neurons 
+        # defining the number of neurons 
         self.cell_size = 100
         
-        # number of hidden layers
+        # setting up the number of hidden layers
         self.hidden_layer = 50
         
         # drop out probability
         self.dropout_probability = [0.3, 0.2]
 
-        # hyperparameters for optimization
+        # setting up the hyperparameters for optimization
         self.loss_decay_rate = 0.96
         self.loss_decay_steps = 180
 
         
-        # initialize all the variables for the CNN
+        # initializing all the variables for the CNN
 
-        # we initialize the placeholder for input whose shape would be (length, width, channel)
+        # initializing the placeholder for input whose shape would be (length, width, channel)
         self.input = tf.placeholder(shape = (self.input_shape[0], self.input_shape[1], self.input_shape[2]), dtype = self.tfcast_type)
         
         # we will also initialize the shape of the target vector whose shape is equal to the number of actions
         self.target_vector = tf.placeholder(shape = (self.num_actions, 1), dtype = self.tfcast_type)
 
-        # initialize feature maps for our corresponding 3 filters
+        # initializing the feature maps for our corresponding 3 filters
         self.features1 = tf.Variable(initial_value = np.random.rand(self.filter_size, self.filter_size, input_shape[2], self.num_filters[0]),
                                      dtype = self.tfcast_type)
         
@@ -98,8 +98,8 @@ class DRQN():
         self.features3 = tf.Variable(initial_value = np.random.rand(self.filter_size, self.filter_size, self.num_filters[1], self.num_filters[2]),
                                      dtype = self.tfcast_type)
 
-        # initialize variables for RNN
-        # recall how RNN works from chapter 7
+        # initializing the variables for RNN
+        
         
         self.h = tf.Variable(initial_value = np.zeros((1, self.cell_size)), dtype = self.tfcast_type)
         
@@ -124,12 +124,12 @@ class DRQN():
                                             high = np.sqrt(6. / (2 * self.cell_size)),
                                             size = (self.cell_size, self.cell_size)),
                               dtype = self.tfcast_type)
-        # bias
+        # setting up the bias
         self.rb = tf.Variable(initial_value = np.zeros(self.cell_size), dtype = self.tfcast_type)
         self.rc = tf.Variable(initial_value = np.zeros(self.cell_size), dtype = self.tfcast_type)
 
         
-        # initialize weights and bias of feed forward network
+        # initializing the weights and bias of feed forward network
         
         # weights
         self.fW = tf.Variable(initial_value = np.random.uniform(
@@ -141,7 +141,7 @@ class DRQN():
         # bias
         self.fb = tf.Variable(initial_value = np.zeros(self.num_actions), dtype = self.tfcast_type)
 
-        # learning rate
+        # defining the learning rate
         self.step_count = tf.Variable(initial_value = 0, dtype = self.tfcast_type)
         self.learning_rate = tf.train.exponential_decay(self.learning_rate, 
                                                    self.step_count,
@@ -152,44 +152,44 @@ class DRQN():
         
         # now let us build the network
 
-        # first convolutional layer
+        # setting up the first convolutional layer
         self.conv1 = tf.nn.conv2d(input = tf.reshape(self.input, shape = (1, self.input_shape[0], self.input_shape[1], self.input_shape[2])), filter = self.features1, strides = [1, self.stride, self.stride, 1], padding = "VALID")
         self.relu1 = tf.nn.relu(self.conv1)
         self.pool1 = tf.nn.max_pool(self.relu1, ksize = [1, self.poolsize, self.poolsize, 1], strides = [1, self.stride, self.stride, 1], padding = "SAME")
 
-        # second convolutional layer
+        # setting up the second convolutional layer
         self.conv2 = tf.nn.conv2d(input = self.pool1, filter = self.features2, strides = [1, self.stride, self.stride, 1], padding = "VALID")
         self.relu2 = tf.nn.relu(self.conv2)
         self.pool2 = tf.nn.max_pool(self.relu2, ksize = [1, self.poolsize, self.poolsize, 1], strides = [1, self.stride, self.stride, 1], padding = "SAME")
 
-        # third convolutional layer
+        # setting up the third convolutional layer
         self.conv3 = tf.nn.conv2d(input = self.pool2, filter = self.features3, strides = [1, self.stride, self.stride, 1], padding = "VALID")
         self.relu3 = tf.nn.relu(self.conv3)
         self.pool3 = tf.nn.max_pool(self.relu3, ksize = [1, self.poolsize, self.poolsize, 1], strides = [1, self.stride, self.stride, 1], padding = "SAME")
 
-        # add dropout and reshape the input
+        # adding the dropout and reshaping the input
         self.drop1 = tf.nn.dropout(self.pool3, self.dropout_probability[0])
         self.reshaped_input = tf.reshape(self.drop1, shape = [1, -1])
 
 
-        # now we build the recurrent neural network, which takes the input from the last layer of the convolutional network
+        # now we build the RNN, which takes the input from the last layer of the convolutional network
         self.h = tf.tanh(tf.matmul(self.reshaped_input, self.rW) + tf.matmul(self.h, self.rU) + self.rb)
         self.o = tf.nn.softmax(tf.matmul(self.h, self.rV) + self.rc)
 
-        # add drop out to RNN
+        # adding the drop out to RNN
         self.drop2 = tf.nn.dropout(self.o, self.dropout_probability[1])
         
         # we feed the result of RNN to the feed forward layer
         self.output = tf.reshape(tf.matmul(self.drop2, self.fW) + self.fb, shape = [-1, 1])
         self.prediction = tf.argmax(self.output)
 
-        # compute loss
+        # computing the loss
         self.loss = tf.reduce_mean(tf.square(self.target_vector - self.output))
         
-        # we use Adam optimizer for minimizing the error
+        # we use the Adam optimizer for minimizing the error
         self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
         
-        # compute gradients of the loss and update the gradients
+        # computing the gradients of the loss and update the gradients
         self.gradients = self.optimizer.compute_gradients(self.loss)
         self.update = self.optimizer.apply_gradients(self.gradients)
 
@@ -213,8 +213,7 @@ class ExperienceReplay():
         # size of the buffer
         self.buffer_size = buffer_size
         
-    # we remove the old transition if the buffer size has reached it's limit. Think off the buffer as a queue, when the new
-    # one comes, the old one goes off
+    # we remove the old transition if the buffer size has reached it's limit. 
     
     def appendToBuffer(self, memory_tuplet):
         if len(self.buffer) > self.buffer_size: 
@@ -223,7 +222,7 @@ class ExperienceReplay():
         self.buffer.append(memory_tuplet) 
         
         
-    # define a function called sample for sampling some random n number of transitions 
+    # defining a function called sample for sampling some random n number of transitions 
     
     def sample(self, n):
         memories = []
